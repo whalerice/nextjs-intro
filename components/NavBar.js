b716b530ed78427bf8cce5a482cb3b383382cab2
whalerice <joyfulforest@gmail.com>
2023-09-08 22:32:19 +0900

    first commit

---
+++ components/NavBar.js
@@ -0,0 +1,10 @@
+import style from "./NavBar.module.css";
+
+export default function NavBar() {
+  return (
+    <nav className="nav">
+      <a href="/">home</a>
+      <a href="/about">about</a>
+    </nav>
+  );
+}
