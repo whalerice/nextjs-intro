d892ee8177db10f6dd0597433e30b4a735671ec0
whalerice <joyfulforest@gmail.com>
2023-09-12 00:20:29 +0900

    #2 PRACTICE PROJECT

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,73 @@
-export default function Home() {
+import Seo from "@/components/Seo";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(`/movies/${title}/${id}`);
+  };
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container">
+      <Seo title="Home" />
+      {results?.map((movie) => {
+        return (
+          <div
+            onClick={() => onClick(movie.id, movie.original_title)}
+            className="movie"
+            key={movie.id}
+          >
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.original_title}
+            />
+            <Link
+              href={`/movies/${movie.original_title}/${movie.id}`}
+              className="link"
+              legacyBehavior
+            >
+              <h4>{movie.original_title}</h4>
+            </Link>
+          </div>
+        );
+      })}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3000/api/movies")
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
---
+++ pages/movies/[...params].js
@@ -0,0 +1,20 @@
+import Seo from "@/components/Seo";
+
+export default function Detail({ params }) {
+  const [title, id] = params || [];
+  return (
+    <div>
+      <Seo title={title} />
+      <h4>{title}</h4>
+    </div>
+  );
+}
+
+export function getServerSideProps({ params: { params } }) {
+  console.log(params);
+  return {
+    props: {
+      params,
+    },
+  };
+}
